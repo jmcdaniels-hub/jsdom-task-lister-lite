ab214f5e9f2281990bb6a0cd4da840940b7e9827
Jade McDaniels <jademcdaniels81@gmail.com>
2021-02-24 17:19:01 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  function createTask(task){
+    let ul = document.getElementById('tasks')
+    let li = document.createElement('li')
+        li.innerText=`${task}`
+
+        var removeTask = document.createElement('input');
+        removeTask.setAttribute('type', 'button');
+        removeTask.setAttribute("value", "x");
+        removeTask.setAttribute("id", "removeButton");
+        removeTask.addEventListener('click', function(e) {
+            li.parentNode.removeChild(li);
+        }, false);
+        li.appendChild(removeTask);
+
+    ul.appendChild(li)
+
+
+  }
+
+
+
+   let description = document.getElementById("new-task-description")
+   let form = document.getElementById("create-task-form");
+       form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        createTask(description.value);
+        e.target.reset();
+        
+
+        
+       });
+
+
 });
+
+
